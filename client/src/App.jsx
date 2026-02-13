70425ea62bfad99c7ac7ecf920c737f18e8053ed
Biswas Sekhar <167892619+BiswasSekhar@users.noreply.github.com>
2026-02-13 17:18:17 +0530

    Switch to HashRouter for Electron file:// compatibility

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import RoomPage from './pages/RoomPage.jsx';
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/room/:roomCode" element={<RoomPage />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
